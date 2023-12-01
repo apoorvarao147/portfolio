da43d57a35c71c549e2ff5c5299ac5054e40ffdd
Apoorva Rao <apoorvarao147@gmail.com>
2023-12-01 13:24:50 -0500

    set menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,33 +1,44 @@
+import { useState } from 'react';
 import {AiOutlineMenu} from 'react-icons/ai';
 
 function Header() {
-  return (
-    <nav className="nav">
-      <NavContent />
 
-      <button>
-        <AiOutlineMenu />
-      </button>
-    </nav>
-  )
-}
+  const [isMenuOpen,setIsMenuOpen] = useState(false);
 
-const NavContent = () => {
   return (
     <>
-    <h2><a href="#home">Apoorva.dev</a></h2>
-    <div>
-      <a href="#home">Home</a>
-      <a href="#about">About</a>
-      <a href="#projects">Projects</a>
-      <a href="#contact">Contact</a>
-    </div>
-    <a href="mailto:apoorvarao147@gmail.com">
+    <nav className="nav">
+      <h2><a href="#home">Apoorva.dev</a></h2>
+
+      <div className={`nav-content ${isMenuOpen ? "display" : "hide"}`}>
+        <ul>
+          <li>
+            <a href="#home" onClick={() => setIsMenuOpen(false)} >Home</a>
+          </li>
+          <li>
+            <a href="#about" onClick={() => setIsMenuOpen(false)} >About</a>
+          </li>
+          <li>
+            <a href="#projects" onClick={() => setIsMenuOpen(false)} >Projects</a>
+          </li>
+          <li>
+            <a href="#contact" onClick={() => setIsMenuOpen(false)} >Contact</a>
+          </li>
+        </ul>     
+      </div>
+
+      {/* <NavContent isMenuOpen={isMenuOpen} /> */}
+ 
+      <button className="side-menu" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+        <AiOutlineMenu />
+      </button>
+
+      <a href="mailto:apoorvarao147@gmail.com">
       <button>Email</button>
-    </a>
+      </a>
+    </nav>
     </>
   )
 }
-
 
 export default Header
