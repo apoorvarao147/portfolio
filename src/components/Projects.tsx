4ad4483dd4ae69448773057bbf4c140e2c5c7d83
Apoorva Rao <apoorvarao147@gmail.com>
2024-01-16 12:49:21 -0500

    described projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -16,20 +16,26 @@ const Projects = () => {
         <div>
           <h3>React Cart</h3>
           <div>
-            <a
-              target="_blank"
-              href="https://github.com/apoorvarao147/react-cart"
-              rel="noreferrer"
-            >
-              Code <FaGithub />
-            </a>
-            <a
-              target="_blank"
-              href="https://develop.d24uct2adzl71u.amplifyapp.com/"
-              rel="noreferrer"
-            >
-              Live Demo <FaAngleDoubleRight />
-            </a>
+            <p>
+              A cart system which is developed using <span>React Hooks</span> and <span>SCSS</span>, providing seamless and intuitive shopping experience. This feature rich system enables users to browse products, add items to their cart, place orders and conveniently track their orders history.
+            </p>
+            <div>
+              <a
+                target="_blank"
+                href="https://github.com/apoorvarao147/react-cart"
+                rel="noreferrer"
+              >
+                Code <FaGithub />
+              </a>
+              <a
+                target="_blank"
+                href="https://develop.d24uct2adzl71u.amplifyapp.com/"
+                rel="noreferrer"
+              >
+                Live Demo <FaAngleDoubleRight />
+              </a>
+            </div>
+            
           </div>
         </div>
       </div>
@@ -38,21 +44,26 @@ const Projects = () => {
         <div>
           <h3>Portfolio</h3>
           <div>
-            <a
-              target="_blank"
-              href="https://github.com/apoorvarao147/portfolio"
-              rel="noreferrer"
-            >
-              Code <FaGithub />
-            </a>
-            <a
-              target="_blank"
-              href="https://www.apoorvarao.com/"
-              rel="noreferrer"
-            >
-              Live Demo <FaAngleDoubleRight />
-            </a>
-          </div>
+              <p>
+                It is a fully interactive and responsive website which was developed using <span>React</span> and <span>SCSS</span>.
+              </p>
+              <div>
+                <a
+                  target="_blank"
+                  href="https://github.com/apoorvarao147/portfolio"
+                  rel="noreferrer"
+                >
+                  Code <FaGithub />
+                </a>
+                <a
+                  target="_blank"
+                  href="https://www.apoorvarao.com/"
+                  rel="noreferrer"
+                >
+                  Live Demo <FaAngleDoubleRight />
+                </a>
+              </div>             
+            </div>          
         </div>
       </div>
       <div className="mini-projects">
@@ -61,20 +72,26 @@ const Projects = () => {
           <div>
             <h3>Hangman Game</h3>
             <div>
-              <a
-                  target="_blank"
-                  href="https://github.com/apoorvarao147/miscellaneous"
-                  rel="noreferrer"
-                >
-                  Code <FaGithub />
-                </a>
+              <p>
+                Try this word-guessing game which was developed using <span>Typescript</span> and <span>React</span>.
+              </p>
+              <div>
                 <a
-                  target="_blank"
-                  href="https://main.d34pqu7igyh8rv.amplifyapp.com/"
-                  rel="noreferrer"
-                >
-                  Live Demo <FaAngleDoubleRight />
-                </a>
+                    target="_blank"
+                    href="https://github.com/apoorvarao147/miscellaneous"
+                    rel="noreferrer"
+                  >
+                    Code <FaGithub />
+                  </a>
+                  <a
+                    target="_blank"
+                    href="https://main.d34pqu7igyh8rv.amplifyapp.com/"
+                    rel="noreferrer"
+                  >
+                    Live Demo <FaAngleDoubleRight />
+                  </a>
+              </div>
+              
             </div>
               
           </div>
@@ -85,20 +102,25 @@ const Projects = () => {
           <div>
             <h3>To Do App</h3>
             <div>
-              <a
-                  target="_blank"
-                  href="https://github.com/apoorvarao147/miscellaneous"
-                  rel="noreferrer"
-                >
-                  Code <FaGithub />
-                </a>
+              <p>
+                Check out this app that allows users to add and delete tasks, they can also save different lists. The style was given using <span>styled-components</span> and developed using <span>React</span>.
+              </p>
+              <div>
                 <a
-                target="_blank"
-                href="https://main.d34pqu7igyh8rv.amplifyapp.com/todo"
-                rel="noreferrer"
-                >
-                  Live Demo <FaAngleDoubleRight />
-                </a>
+                    target="_blank"
+                    href="https://github.com/apoorvarao147/miscellaneous"
+                    rel="noreferrer"
+                  >
+                    Code <FaGithub />
+                  </a>
+                  <a
+                  target="_blank"
+                  href="https://main.d34pqu7igyh8rv.amplifyapp.com/todo"
+                  rel="noreferrer"
+                  >
+                    Live Demo <FaAngleDoubleRight />
+                  </a>
+              </div>             
             </div>
              
           </div>
